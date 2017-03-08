7c3e61ce49f999c3e4a49062410f2c0874f95f65
Therk <therk@me.com>
2017-03-08 08:26:09 -0500

    Converted from static dat to using json-server

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,5 @@
 const graphql = require('graphql');
-
-const _ = require('lodash');
+const axios = require('axios');
 
 const {
 	GraphQLObjectType,
@@ -9,17 +8,22 @@ const {
 	GraphQLSchema
 } = graphql;
 
-const users = [	
-	{ id: '23', firstName: 'Bill', age: 20 },
-	{ id: '47', firstName: 'Samantha', age: 21 }
-];
+const CompanyType =  new GraphQLObjectType({
+	name: 'Company',
+	fields: {
+		id: { type: GraphQLString },
+		name: { type: GraphQLString },
+		description: { type: GraphQLString }
+	}
+});
 
 const UserType =  new GraphQLObjectType({
 	name: 'User',
 	fields: {
 		id: { type: GraphQLString },
 		firstName: { type: GraphQLString },
-		age: { type: GraphQLInt }
+		age: { type: GraphQLInt },
+		companyId: { type: GraphQLString }
 	}
 });
 
@@ -30,7 +34,8 @@ const RootQuery = new GraphQLObjectType ({
 			type: UserType,
 			args: { id: { type: GraphQLString }},
 			resolve(parentValue, args) {
-				return _.find(users, { id: args.id });
+				return axios.get(`http://localhost:3000/users/${args.id}`)
+					.then(resp => resp.data); // pairing down the response to data element
 			}
 		}
 	}
