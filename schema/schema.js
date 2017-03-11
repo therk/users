36ac56825c718633fb779ae96cfd7852e218d7b2
Therk <therk@me.com>
2017-03-11 15:47:40 -0500

    Added support to query company and its users

--- schema/schema.js
+++ schema/schema.js
@@ -5,21 +5,29 @@ const {
 	GraphQLObjectType,
 	GraphQLString,
 	GraphQLInt,
-	GraphQLSchema
+	GraphQLSchema,
+	GraphQLList
 } = graphql;
 
 const CompanyType =  new GraphQLObjectType({
 	name: 'Company',
-	fields: {
+	fields: () => ({
 		id: { type: GraphQLString },
 		name: { type: GraphQLString },
-		description: { type: GraphQLString }
-	}
+		description: { type: GraphQLString },
+		users: {
+			type: new GraphQLList(UserType),
+			resolve(parentValue, args){
+				return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+				.then(res => res.data);
+			}
+		}
+	})
 });
 
 const UserType =  new GraphQLObjectType({
 	name: 'User',
-	fields: {
+	fields: () => ({
 		id: { type: GraphQLString },
 		firstName: { type: GraphQLString },
 		age: { type: GraphQLInt },
@@ -27,21 +35,29 @@ const UserType =  new GraphQLObjectType({
 			type: CompanyType,
 			resolve(parentValue, args) {
 				return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
-					.then(res => res.data);
+				.then(res => res.data);
 			} 
 		}
-	}
+	})
 });
 
 const RootQuery = new GraphQLObjectType ({
-		name: 'RootQueryType',
+	name: 'RootQueryType',
 	fields: {
 		user: {
 			type: UserType,
 			args: { id: { type: GraphQLString }},
 			resolve(parentValue, args) {
 				return axios.get(`http://localhost:3000/users/${args.id}`)
-					.then(resp => resp.data); // pairing down the response to data element
+				.then(resp => resp.data); // pairing down the response to data element
+			}
+		},
+		company: {
+			type: CompanyType,
+			args: { id: { type: GraphQLString }},
+			resolve(parentValue, args) {
+				return axios.get(`http://localhost:3000/companies/${args.id}`)
+				.then(resp => resp.data); // pairing down the response to data element
 			}
 		}
 	}
